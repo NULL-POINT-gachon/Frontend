f3f1bccaca090ad4fbe807840b6f34f6fc84fc72
Seol <pseol@gachon.ac.kr>
2025-02-27 20:32:24 +0900

    Add React Router and update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Header from "./components/Header";
 import "./styles/global.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <p>Welcome to my React project!</p>
-    </div>
+    <Router>
+      <div>
+        <h1>My React App</h1>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
