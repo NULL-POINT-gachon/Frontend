76a79552ebc29b2b990ace1f65ee39586f9aa0b5
Seol <pseol@gachon.ac.kr>
2025-05-01 22:56:43 +0900

    ✨ [Admin] 계정 설정 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,8 @@ import AdminSchedules from "./components/admin/AdminSchedules";
 
 import AdminAIPage from "./components/admin/AdminAIPage";
 
+import AdminAccountSetting from "./components/admin/AdminAccountSetting";
+
 import { TravelProvider } from "./contexts/TravelContext";
 import { AuthProvider } from "./contexts/AuthContext";
 
@@ -181,6 +183,8 @@ function App() {
             <Route path="schedules" element={<AdminSchedules />} />
 
             <Route path="ai" element={<AdminAIPage />} />
+
+            <Route path="account" element={<AdminAccountSetting />} />
 
             </Route>
             </Route>
